6c4cf41691601b203bd69564d06721cff4f329b2
David1997sb <salasbar97@gmail.com>
2017-04-08 15:06:32 -0600

    Cambios
    Se mejoro la estetica del programa

--- src/sistemadetickets/Admin.java
+++ src/sistemadetickets/Admin.java
@@ -19,43 +19,17 @@ public class Admin {
         Usuario Users2 =new Usuario("Elizabeth", "Cano", id++);
         Usuario Users3 =new Usuario("Fabian", "Hernandez", id++);
         Usuario Users4 =new Usuario("David", "Salas", id++);
-        
-        
 
     public Admin() {
         list=new ArrayList<>();
         
     }
     
-    
     public void llamaUsuarios(){
-       
     ListaUsuarios lista= new ListaUsuarios(null, null, id);
-    
-    
-    
-    
-    
     lista.currentUsser();
     lista.printList(16836);
-   
-    
-    
-    
-    
-    
-    
-    
-    
     }
     public void imprimeLista(){
-    
-    
-    
     }
-    
-        
-        
-
-   
 }
